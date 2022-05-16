d23dddc0a85172c37a3699b534727069aa10cce1
Henning Funke <henning.funke@gdata.de>
2022-05-16 08:57:51 +0200

    remove deprecated function

--- src/util/string.h
+++ src/util/string.h
@@ -1,21 +1,18 @@
 #pragma once
 
-
 #include <string>
 
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
 
 static inline void rtrim(std::string& s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
         return !std::isspace(ch);
     }).base(), s.end());
 }
-
-
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
-}
-
 
 std::string remove_extra_whitespaces ( const std::string &input ) {
     std::string output;
